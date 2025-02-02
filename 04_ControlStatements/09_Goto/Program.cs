2c0aa31d8215bc775bb63deed6c22292726ee103
zmercury <messiniki@outlook.com>
2025-02-02 15:02:43 +0545

    Control Structure

---
+++ 04_ControlStatements/09_Goto/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _09_Goto;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    ineligible:
+        Console.WriteLine("You cant cast a vote");
+
+        Console.WriteLine("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age > 18)
+        {
+            Console.WriteLine("You can cast vote!");
+        }
+
+        else
+        {
+            goto ineligible;
+        }
+
+    }
+}
