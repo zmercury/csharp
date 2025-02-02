9ad92bd2227ee4be314e19b6f0d956f551302cd0
zmercury <messiniki@outlook.com>
2025-02-02 15:47:35 +0545

    Type Conversion | Const | Struct

---
+++ 07_ConstructorDestructor/c_destructor/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace c_destructor;
+
+class Employee
+{
+    public Employee()
+    {
+        Console.WriteLine("Constructor called.");
+    }
+
+    // not executed though destroys the object call.
+    ~Employee()
+    {
+        Console.WriteLine("Destructor called.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee e1 = new Employee();
+    }
+}
