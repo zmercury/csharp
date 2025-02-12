65edf1b57f0c8df2fb9f15de970663b1c1d69115
zmercury <messiniki@outlook.com>
2025-02-12 19:23:11 +0545

    Assignment

---
+++ 00_Temp/02_InterfaceArith/Program.cs
@@ -0,0 +1,63 @@
+﻿// Demonstrate the use of interface for basic arithmetic operations on two user input
+namespace _02_InterfaceArith;
+using System;
+
+public interface IArithmetic
+{
+    public int add(int a, int b);
+    public int sub(int a, int b);
+    public int div(int a, int b);
+    public int mul(int a, int b);
+}
+
+class Program : IArithmetic
+{
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int sub(int a, int b)
+    {
+        if (a > b)
+        {
+            return (a - b);
+        }
+        else
+        {
+            return (b - a);
+        }
+    }
+
+    public int div(int a, int b)
+    {
+        if (a > b)
+        {
+            return (a / b);
+        }
+        else
+        {
+            return (b / a);
+        }
+    }
+
+    public int mul(int a, int b)
+    {
+        return (a * b);
+    }
+
+    static void Main(string[] args)
+    {
+        IArithmetic ar = new Program();
+
+        Console.WriteLine("Enter the value of a: ");
+        int numOne = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the value of b: ");
+        int numTwo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sum = " + ar.add(numOne, numTwo));
+        Console.WriteLine("Substraction = " + ar.sub(numOne, numTwo));
+        Console.WriteLine("Division = " + ar.div(numOne, numTwo));
+        Console.WriteLine("Multiplication = " + ar.mul(numOne, numTwo));
+    }
+}
--- 03_DataTypes/06_ReferenceTypeObject/obj/Debug/net9.0/06_ReferenceTypeObject.AssemblyInfo.cs
+++ 03_DataTypes/06_ReferenceTypeObject/obj/Debug/net9.0/06_ReferenceTypeObject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("06_ReferenceTypeObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc045a4ae5a6161dc2fa354630866b6c731f111")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a86f4551fb2f7e2b82947fda26a7fdb419806cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("06_ReferenceTypeObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("06_ReferenceTypeObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
