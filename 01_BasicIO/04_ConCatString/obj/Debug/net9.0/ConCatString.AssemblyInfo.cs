878d2a76f7b0ae801c2a6e1ab0261fa750bbfee6
zmercury <messiniki@outlook.com>
2025-02-17 08:40:43 +0545

    Lambda

--- 01_BasicIO/04_ConCatString/obj/Debug/net9.0/ConCatString.AssemblyInfo.cs
+++ 01_BasicIO/04_ConCatString/obj/Debug/net9.0/ConCatString.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConCatString")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d34d3253da6570a0c53ad05a4b8f4474bf7d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f980a9c7202e73feb2f5dc8a3f664dc90bda1ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConCatString")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConCatString")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 19_LambdaExpression/06_lambda/Program.cs
+++ 19_LambdaExpression/06_lambda/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace _06_lambda;
+﻿//Trying without using delegate
+//Lambda type: Statement Lambda
+namespace _06_lambda;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var calcSum = (int a, int b) =>
+        {
+            int sum = (a + b);
+            return sum;
+        };
+
+        Console.WriteLine("The sum is " + calcSum(5, 3));
     }
 }
