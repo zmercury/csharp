7d2d02e6d252252a4c2ed4e65e01383feb7313d3
zmercury <messiniki@outlook.com>
2025-03-25 08:22:53 +0545

    Sorted list string

--- 07_ConstructorDestructor/a_constructor/obj/Debug/net9.0/a_constructor.AssemblyInfo.cs
+++ 07_ConstructorDestructor/a_constructor/obj/Debug/net9.0/a_constructor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("a_constructor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fbb0a360b10176785997f0206fc1b0c63e72e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a20764624896de24658b34bf10baa702402247")]
 [assembly: System.Reflection.AssemblyProductAttribute("a_constructor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("a_constructor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 25_Generics/n_sortedList_tk_tv_string/Program.cs
+++ 25_Generics/n_sortedList_tk_tv_string/Program.cs
@@ -1,9 +1,19 @@
 ﻿namespace n_sortedList_tk_tv_string;
 
+#nullable disable
+
+using System;
+
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+  static void Main(string[] args)
+  {
+    SortedList<string, string> cities = new SortedList<string, string>(){
+      {"Nepal", "Kathmandu"},
+      {"India", "New Delhi"},
+      {"Japan", "Tokyo"},
+    };
+
+    cities.ToList().ForEach(items => Console.WriteLine($"Key is {items.Key} and value is {items.Value}"));
+  }
 }
